f4dc8b7cdc62e2de7c15fcae74483deabd2e1b3b
Gregory Schern <schern.gregory@gmail.com>
2016-01-05 03:43:39 +0000

    Install simpleform and implement form

--- app/controllers/itches_controller.rb
+++ app/controllers/itches_controller.rb
@@ -1,5 +1,18 @@
 class ItchesController < ApplicationController
 	def index
 		@itches = Itch.all
+		@itch = Itch.new
 	end
+
+	def create
+		Itch.create(itch_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def itch_params
+		params.require(:itch).permit(:name)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Scratch::Application.routes.draw do
 
   root 'itches#index'
+  resources :itches, :only => [:create]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
