51e9b2ddb223460f1656263990727e01e4093554
Gregory Schern <schern.gregory@gmail.com>
2016-01-05 01:33:51 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scratch::Application.config.secret_key_base = 'baeb182469d4194a1efc6e13993f866f4bc0325622f291b4538eb7c50000e1b55433ff0c055b23df9f55ece0bd40dc01720bd873dd8760bd5568642683d47e5e'
